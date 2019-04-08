7d395175781a87c630f2694f417e72163a28ff45
tiehanlin <476494344@qq.com>
2019-04-08 17:34:12 +0800

    day01done

--- proShopCart/src/components/proShopCartDemo.vue
+++ proShopCart/src/components/proShopCartDemo.vue
@@ -1,37 +1,80 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-		<input type="text" class="leftDiv" placeholder="请输入" />
-		<div class="rightDiv"></div>
-		<van-button type="info">信息按钮</van-button>
-  </div>
+	<div>
+		<h1>{{ msg }}</h1>
+		<div class="wrapDiv">
+			<input type="text" class="leftDiv" ref="inputRef" placeholder="请输入" />
+			<div class="rightDiv">{{txt_data}}</div>
+		</div>
+		<van-button type="info" @click="sendBtn">发送</van-button>
+	<van-button type="info" @click="getBtn">获取</van-button>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'proShopCartDemo',
-  data () {
-    return {
-      msg: '铁铁铁'
-    }
-  }
-}
+	import axios from 'axios'
+	export default {
+		name: 'proShopCartDemo',
+		data() {
+			return {
+				msg: '铁铁铁',
+				txt_data:''
+			}
+		},
+		methods:{
+			sendBtn(){
+				let _val=this.$refs.inputRef.value;
+				//console.log(_val)
+				axios.get("http://localhost:5678/node_a",{
+					params:{
+						xxval:_val
+					}
+				});
+			},
+			getBtn(){
+				axios.get("http://localhost:5678/node_b").then(_d=>{
+					console.log(_d.data)
+					this.txt_data=_d.data
+				});
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+	    .wrapDiv{
+	  width: 80%;height: 100px;overflow: hidden;border: 1px solid #666;
+	  background: #eee;border-radius: 10px;
+	  margin:10px auto;
+	}
+	.leftDiv{
+	  float: left;width: 40%;margin:10px;
+	  border-radius: 10px;height: 30px;border:1px solid #999;
+	  padding:10px;line-height: 30px;text-align: center;
+	  font-size: 22px;
+	}
+	.rightDiv{
+	  float: right;width: 40%;margin:10px;
+	  border-radius: 10px;height: 30px;border:1px solid #999;
+	  padding:10px;background: #fff;line-height: 30px;text-align: center;
+	  font-size: 22px;
+	}
 </style>
--- 中间件/api_dev.js
+++ 中间件/api_dev.js
@@ -1,11 +1,26 @@
 var express =require('express');
 var app=express();
-
-//第一个nodejs接口
-app.get("/xxx",function(req,res){
-	console.log(("xxx,start"))
+// 解决跨域问题，网上成熟的解决方案，我是直接拷贝的
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+//用来临时存数据
+var _xxObj=null;
+//接受
+app.get("/node_a",function(req,res){
+	console.log(req.query.xxval)
+	_xxObj=req.query;
 	res.end();
+})
+//发送
+app.get("/node_b",function(req,res){
+	res.send(_xxObj.xxval+"tietietie");
 })
 app.listen(5678,function(){
 	console.log("5678,中间启动")
-});
+})
